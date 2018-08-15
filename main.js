371a813e74c11c7fa8ccaf30bf0399d206e1e7e0
@snailsack <nickrailsback@gmail.com>
2018-08-15 14:46:19 -0400

    Got the second fetch to work!

--- main.js
+++ main.js
@@ -8,25 +8,46 @@ function getAllBills() {
   });
 }
 
-function displayResults(someResults) {
+
+
+function displayResults(results) {
 
   for (let i = 0; i < results.length; i++) {
+    
+    let x = results[i];
     const content = `
-    <div className="billcard">
-      <h1 className="card-bill-id">${results[i].bill_id}</h1>
-      <h3 className="bill-type">${results[i].title}</h3>
+    <div className="billcard" data-bill-id=${x.id}>
+      <h1 className="card-bill-id">${x.bill_id}</h1>
+      <h3 className="bill-type">${x.title}</h3>
       <button className="bill-details">Bill Details</button>
     </div>
     `;
     const element = document.createElement("div");
     const allCards = document.getElementById("billDisplay");
     element.innerHTML = content;
-    allCards.appendChild(element)
+    allCards.appendChild(element);
+    element.addEventListener('click', getBillDetails);
   }
-
-  console.log(results);
 }
 
+function secondFetch(billId) {
+  fetch(`https://openstates.org/api/v1/bills/${billId}/?apikey=2a939a8d-1448-4810-b036-79139a6a7f33&format=json`)
+    .then(res => res.json())
+    .then(result => {
+        console.log(result);
+        console.log('DONE');
+      }
+    );
+}
+
+function getBillDetails(event) {
+  let element = event.target;
+  let parent = element.parentElement;
+  console.log(parent.dataset.billId);
+  secondFetch(parent.dataset.billId);
+ }
+
+//User search for bills
 function search() {
   getAllBills().then(currentSessionBills => {
     document.getElementById("billDisplay").textContent = "";
@@ -36,7 +57,7 @@ function search() {
       shouldSort: true,
       tokenize: true,
       // distance: 50,
-      threshold: 0.4,
+      threshold: 0.5,
       // location: 0,
       // minMatchCharLength: 1,
       // maxPatternLength: 32,
@@ -48,15 +69,13 @@ function search() {
 
     let fuse = new Fuse(currentSessionBills, options);
 
-    results = fuse.search(searchTerms);
-    displayResults(results);
-
-    return searchResults;
+    return displayResults(fuse.search(searchTerms));
   });
 };
 
 let input = document.getElementById("billSearch");
 
+//User press Enter key
 input.addEventListener("keyup", function (event) {
   event.preventDefault();
   if (event.keyCode === 13) {
